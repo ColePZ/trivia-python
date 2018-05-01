ed9740bdad655e38061dc0520a400e4f6cdaccd3
Cole Pazoid <34737903+ColePZ@users.noreply.github.com>
2018-05-01 19:36:49 +0100

    Core Stuff
    Added the Core Stuff to the Master Branch, first command. 
    This has NOT been tested yet and may not work.

---
+++ game.py
@@ -0,0 +1,12 @@
+question_num = ""
+answer = ""
+print("Trivia by ColePZ/HiddenCale")
+print("Python 3.6.3")
+
+question_num = input("Question Number: ")
+
+if question_num == "1":
+ print("What age is Overwatch?")
+ answer = input("Answer: ")
+  if answer = 12:
+   print("Correct!")
