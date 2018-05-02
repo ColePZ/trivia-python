00b71ad22fea622832a28a407ef18f7d0cc10f1a
Cole Pazoid <34737903+ColePZ@users.noreply.github.com>
2018-05-02 18:28:59 +0100

    Added Questions
    Added some more questions

--- game.py
+++ game.py
@@ -8,5 +8,41 @@ question_num = input("Question Number: ")
 if question_num == "1":
  print("What age is Overwatch?")
  answer = input("Answer: ")
-  if answer = 12:
+  if answer == 12:
    print("Correct!")
+   
+if question_num == "2":
+ print("When was Minecraft Made?")
+ answer = input("Answer: ")
+  if answer == "2009":
+   print("Correct!")
+   
+if question_num == "3":
+ print("When was Roblox Made?")
+ answer = input("Answer: ")
+  if answer == "2006":
+   print("Correct!")
+   
+if question_num == "4":
+ print("What version is Geometry Dash at the moment? [2/5/18]")
+ answer = input("Answer: ")
+  if answer == "2.11":
+   print("Correct!")
+   
+if question_num == "5":
+ print("Who was this Trivia by?")
+ print("A: HiddenCale/ColePZ")
+ print("B: Victor Tran/vicr123")
+ print("C: Cat/cat-the-coder")
+ answer = input("Answer: ")
+  if answer == "A":
+   print("Correct!")
+   
+   
+ if question_num == "6":
+  print("Is Fortnite an Open Beta [A] or a Closed Beta [B]")
+   answer = input("Answer: ")
+    if answer == "B":
+     print("Correct!")
+    
+ 
